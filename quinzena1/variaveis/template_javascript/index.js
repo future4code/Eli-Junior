7e924d7f83b5ce19d2c53f2952c3b0d2210f3e1a
Eli Tosta Junior <eli_gtj@hotmail.com>
2021-09-08 21:45:46 -0300

    respostas-da-atividade-aula-3

---
+++ quinzena1/variaveis/template_javascript/index.js
@@ -0,0 +1,93 @@
+// === Exercícios de interpretação de código ===
+
+// === QUESTÃO 1 ===
+
+// let a = 10
+// let b = 10
+
+// console.log(b)
+// resultado = 10
+
+// b = 5
+// console.log(a, b)
+// resultado = 10, 5
+
+// === QUESTÃO 2 ===
+
+// let a = 10
+// let b = 20
+// let c
+// c = a
+// b = c
+// a = b
+
+// console.log(a, b, c)
+// resultado = 20, c, 10
+
+// === QUESTÃO 3 ===
+
+// let p = prompt("Quantas horas você trabalha por dia?")
+// let t = prompt("Quanto você recebe por dia?")
+// console.log(`Voce recebe ${t/p} por hora`)
+
+// === Exercícios de escrita de código ===
+
+// === QUESTÃO 1 ===
+
+// let nome 
+// let idade
+
+// console.log(typeof nome)
+// console.log(typeof idade)
+
+// resposta nos dois = undefined
+// Este tipo de código foi impresso porque não foi colocado nenhum valor nas variáveis, sendo impossível saber o tipo deles.
+
+// const nome = prompt("Qual é o seu nome?")
+// let idade = prompt("Qual é a sua idade?")
+
+// console.log("O seu nome é", nome)
+// console.log("Você tem", idade, "anos de idade")
+
+// O seu nome é Eli
+// Você tem 28 anos de idade
+
+// Foi impresso as variáveis que eu digitei nas perguntas, e ao imprimir, a resposta foi encaixada no texto.
+
+// console.log("Olá", nome, ", você tem", idade, "anos")
+
+// Olá Eli , você tem 28 anos
+
+// === QUESTÃO 2 ===
+
+// const valor1 = prompt("Você está usando uma roupa azul hoje?")
+// const valor2 = prompt("Fez calor no dia de hoje?")
+// const valor3 = prompt("Você gosta de café?")
+
+// console.log("Você está usando uma roupa azul hoje? -", valor1)
+// console.log("Fez calor no dia de hoje? -", valor2)
+// console.log("Você gosta de café? -", valor3)
+
+// Você está usando uma roupa azul hoje? - NÃO
+// Fez calor no dia de hoje? - SIM
+// Você gosta de café? - SIM
+
+// === QUESTÃO 3 ===
+
+// let a = 10
+// let b = 25
+// let c = 0
+
+// c = a
+// a = b
+// b = c
+
+// console.log("O novo valor de a é", a)
+// console.log("O novo valor de b é", b)
+
+// O novo valor de a é 25
+// O novo valor de b é 10
+
+
+
+
