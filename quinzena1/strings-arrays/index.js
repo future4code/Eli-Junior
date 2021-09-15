1f66863e251911398a6b570ab4f3cc472c78dab0
Eli Tosta Junior <eli_gtj@hotmail.com>
2021-09-14 21:37:19 -0300

    atividade-aula-05

---
+++ quinzena1/strings-arrays/index.js
@@ -0,0 +1,93 @@
+// === Exercícios de interpretação de código ===
+
+// === QUESTÃO 01 ===
+
+// let array
+// console.log('a. ', array)
+
+// array = null
+// console.log('b. ', array)
+
+// array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// console.log('c. ', array.length)
+
+// let i = 0
+// console.log('d. ', array[i])
+
+// array[i+1] = 19
+// console.log('e. ', array)
+
+// const valor = array[i+6]
+// console.log('f. ', valor)
+
+// a. undefined
+// b. null
+// c. 11
+// d. 0
+// e. 19
+// f. 6
+
+// === QUESTÃO 02 ===
+
+// const frase = prompt("Digite uma frase")
+
+// console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+
+// SUBI NUM ÔNIBUS EM MIRROCOS 27
+
+// As letras ficam maiúsculas, substitui as letras "a" em letra "i", além
+// de dizer qual é o tamanho das strings.
+
+// === Exercícios de escrita de código ===
+
+// === QUESTÃO 01 ===
+
+// const nomeDoUsuario = prompt("Qual é o seu nome?")
+// const emailDoUsuario = prompt("Qual é o seu e-mail?")
+
+// console.log(`O e-mail ${emailDoUsuario} foi cadastrado com sucesso. Seja bem-vinda(o), ${nomeDoUsuario}!`)
+
+// O e-mail eli@hotmail.com foi cadastrado com sucesso. Seja bem-vinda(o), Eli!
+
+// === QUESTÃO 02 ===
+
+// const comidasPreferidas = ["hamburger", "pizza", "churrasco", "shawarma", "espaguete"]
+
+// console.log(comidasPreferidas)
+// console.log(`Essas são minhas comidas preferidas: ${comidasPreferidas}`)
+
+// const perguntarComidaPreferida = prompt("Qual é a sua comida preferida?")
+
+// a) (5) ['hamburger', 'pizza', 'churrasco', 'shawarma', 'espaguete']
+// b) Essas são minhas comidas preferidas: hamburger,pizza,churrasco,shawarma,espaguete
+
+// c) comidasPreferidas[1] = perguntarComidaPreferida
+// console.log(comidasPreferidas)
+
+// 0: "hamburger", 1: "lasanha", 2: "churrasco", 3: "shawarma", 4: "espaguete"
+
+// === QUESTÃO 03 ===
+
+// a) let listaDeTarefas = []
+
+// const tarefa1 = prompt("Coloque aqui sua primeira tarefa do dia")
+// const tarefa2 = prompt("Coloque aqui sua segunda tarefa do dia")
+// const tarefa3 = prompt("Coloque aqui sua terceira tarefa do dia")
+
+// let listaDeTarefas = [tarefa1, tarefa2, tarefa3]
+// console.log(listaDeTarefas)
+
+// c) (3) ['ler livro', 'estudar programação', 'estudar inglês']
+
+// d) const indiceTarefa = Number(prompt("Digite o índice de uma tarefa já realizada: digite 0, 1 ou 2"))
+// console.log(tarefa1 || tarefa2 || tarefa3) // 0
+
+// e) const removerItem = listaDeTarefas.splice(indiceTarefa, 1)
+// console.log(listaDeTarefas)
+
+// f) (2) ['estudar programação', 'estudar inglês']
+
+
+
+
+
