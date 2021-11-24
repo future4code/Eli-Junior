fdd5948d4ba393787a01663cdd54fcefe677ae57
Eli Tosta Junior <eli_gtj@hotmail.com>
2021-11-23 23:22:55 -0300

    fiz a atividade que consegui fazer

---
+++ rend-condicional-form/meu-formulario/src/components/Etapa2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Etapa2() {
+  return <div>
+      <h1>ETAPA 2 - INFORMAÇÕES DE ENSINO SUPERIOR</h1>
+      <h3>5. Qual curso?</h3>
+      <h3>6. Qual unidade de ensino?</h3>
+    </div>
+}
+
+export default Etapa2;
---
+++ rend-condicional-form/meu-formulario/src/components/Final.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+function Final() {
+  return <div>
+      <h1>O FORMULÁRIO ACABOU</h1>
+      <h3>Muito obrigado por participar! Entraremos em contato!</h3>
+    </div>
+}
+
+export default Final;
