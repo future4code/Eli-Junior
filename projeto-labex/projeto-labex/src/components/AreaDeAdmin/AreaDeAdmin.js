53f85770153233e3d3d9f53a9397fff9fa57f9b5
Eli Tosta Junior <eli_gtj@hotmail.com>
2022-01-20 23:28:18 -0300

    feito parte inicial do projeto

---
+++ projeto-labex/projeto-labex/src/components/AreaDeAdmin/AreaDeAdmin.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default class AreaDeAdmin extends React.Component {
+  render() {
+    return (
+        <div>
+            <h1>Login</h1>
+            {/* <form action="">
+              <div>
+                <label for="nome">Nome</label>
+                <input type="text" id="nome">
+              </div>
+              <div>
+                <label for="email">E-mail</label>
+                <input type="email" id="email">
+              </div> */}
+            <button>Voltar</button>
+            <button>Entrar</button>
+        </div>
+    );
+  }
+}
+
---
+++ projeto-labex/projeto-labex/src/components/VerViagens/VerViagens.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class VerViagens extends React.Component {
+  render() {
+    return (
+        <div>
+            <button>Voltar</button>
+            <button>Inscrever-se</button>
+            <h1>Lista de Viagens</h1>
+        </div>
+    );
+  }
+}
+
