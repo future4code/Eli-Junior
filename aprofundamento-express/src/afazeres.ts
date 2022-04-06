7baaefb634b3b368fb4c3c9fa443f391cdd20583
eligtj <eli_gtj@hotmail.com>
2022-04-05 23:12:32 -0300

    feito atividade até exercício 4

---
+++ aprofundamento-express/src/afazeres.ts
@@ -0,0 +1,43 @@
+export type Afazeres = {
+    userId: number,
+    title: string,
+    completed: boolean
+}
+
+export const listaAfazeres: Afazeres[] = [
+    {
+        "userId": 1,
+        "title": "delectus aut autem",
+        "completed": false
+    },
+    {
+        "userId": 2,
+        "title": "quis ut nam facilis et officia qui",
+        "completed": false
+    },
+    {
+        "userId": 3,
+        "title": "fugiat veniam minus",
+        "completed": false
+    },
+    {
+        "userId": 4,
+        "title": "et porro tempora",
+        "completed": true
+    },
+    {
+        "userId": 5,
+        "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+        "completed": false
+    },
+    {
+        "userId": 6,
+        "title": "quo adipisci enim quam ut ab",
+        "completed": true
+    },
+    {
+        "userId": 7,
+        "title": "molestiae perspiciatis ipsa",
+        "completed": false
+    }
+]
