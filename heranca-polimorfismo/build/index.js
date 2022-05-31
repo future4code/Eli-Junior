81ba5c3601211699c10a249b4480743f18525c75
eligtj <eli_gtj@hotmail.com>
2022-05-31 14:50:55 -0300

    Revert "instalado template do exercício"
    This reverts commit 711ac3bb13a9f34245c6bda871b0047200fe2f5c.

--- heranca-polimorfismo/build/index.js
+++ heranca-polimorfismo/build/index.js
@@ -1,3 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const User_1 = require("./classes/User");
+const insertUser = new User_1.User("1", "bananinha@labenu.com.br", "Bananinha", "abcdefg");
+console.log(insertUser);
 //# sourceMappingURL=index.js.map
--- heranca-polimorfismo/src/index.ts
+++ heranca-polimorfismo/src/index.ts
@@ -1,1 +1,4 @@
 import { User } from "./classes/User";
+
+const insertUser = new User("1", "bananinha@labenu.com.br", "Bananinha", "abcdefg")
+console.log(insertUser)
